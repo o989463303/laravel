cadd4c9eebdeab20cd40b50070196b4d1ab7dfb6
o989463303 <o989463303@gmail.com>
2020-12-16 22:03:03 +0800

    first commit

---
+++ resources/views/notes.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes</title>
+</head>
+<body>
+    <h1>Notes</h1>
+    <ul>
+        <li>HTML5</li>
+        <li>CSS3</li>
+        <li>Javascriipt</li>
+        <li>Docker</li>
+        <li>Ubuntu</li>
+        <li>Apache2</li>
+        <li>PHP</li>
+        <li>MySQL</li>
+        <li>Laravel</li>
+        <li>AWS</li>
+        <li>Windows Server</li>
+        <li>Exchage Server</li>
+        <li>Windows 7/10</li>
+        <li>Microsoft Excel</li>
+        <li>Microsoft Word</li>
+        <li>Microsoft Powerpoint</li>
+        <li>Microsoft Access</li>
+        <li>Skype</li>
+        <li>Microsoft Visual Studio Code</li>
+        <li>Microsoft Visual Studio</li>
+        <li>電腦</li>
+        <li>網路</li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</body>
+</html>
